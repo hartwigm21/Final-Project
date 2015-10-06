2a86a1c4cf426bf2d6f8030da8c92f7d34dce96c
hartwigm21 <Hartwigm21@gmail.com>
2015-10-06 13:26:58 -0400

    Initial Commit

---
+++ Forgetting SherlockTests/Forgetting_SherlockTests.swift
@@ -0,0 +1,36 @@
+//
+//  Forgetting_SherlockTests.swift
+//  Forgetting SherlockTests
+//
+//  Created by Matthew Hartwig on 10/6/15.
+//  Copyright © 2015 Matthew Hartwig. All rights reserved.
+//
+
+import XCTest
+@testable import Forgetting_Sherlock
+
+class Forgetting_SherlockTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
